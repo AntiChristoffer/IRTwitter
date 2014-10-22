816a2d60537e8e2259ac9f0bdb19a833c9e71963
AntiChristoffer <christoffer.dahlgren@gmail.com>
2014-10-22 17:22:23 +0200

    Some parser done, minor fixes required

--- IRTwitter/src/Parser.java
+++ IRTwitter/src/Parser.java
@@ -9,28 +9,68 @@ public class Parser {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		Parser parser = new Parser();
-		parser.run();
+		String fileToRead = "data/testdata.manual.2009.06.14.csv";
+		String fileToWrite = "data/testdataPurged.txt";
+		parser.run(fileToRead, fileToWrite);
+		String split3File = "data/testDataSplit3gram.txt";
+		parser.nGramSplitter(fileToWrite, split3File, 3);
+		String split2File = "data/testDataSplit2gram.txt";
+		parser.nGramSplitter(fileToWrite, split2File, 2);
 	}
 	public void parser() throws IOException{
 		
 		
 	}
 	
-	public void run() throws IOException{
-	try{
-		String fileToRead = "data/testdata.manual.2009.06.14.csv";
-		String fileToWrite = "data/testdataPurged.txt";
-		BufferedReader br = new BufferedReader(new FileReader(fileToRead));
-		FileWriter out = new FileWriter(fileToWrite); 
-		while(br.read() != -1){
+	public void run(String fileToRead, String fileToWrite) throws IOException{
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(fileToRead));
+			FileWriter out = new FileWriter(fileToWrite); 
 			String tempString = br.readLine();
-			String[] tempStringArray = tempString.split("\",\"");
-			out.write(tempStringArray[5].substring(0, tempStringArray[5].length()-1));
-			out.write("\n");
+			while(tempString != null){
+				String[] tempStringArray = tempString.split("\",\"");
+				out.write(tempStringArray[5].substring(0, tempStringArray[5].length()-1));
+				out.write("\n");
+				tempString = br.readLine();
+			}
+			br.close();
+			out.close();
+				
+		} catch(FileNotFoundException e){
+			System.out.println("Exception! " + e);
 		}
-			
-	} catch(FileNotFoundException e){
-		System.out.println("Exception! " + e);
 	}
-}
+	
+	public void nGramSplitter(String fileToRead, String fileToWrite, int orderGram) throws IOException{
+		try{
+			BufferedReader br = new BufferedReader(new FileReader(fileToRead));
+			FileWriter out = new FileWriter(fileToWrite); 
+			String tempString = br.readLine();
+			while(tempString != null){
+				tempString = tempString.replaceAll("[.]{2,}|[,]", "");
+				String[] tempStringArray = tempString.split("\\s{1,}");
+				for(int i = 0; i<tempStringArray.length; i++){
+					System.out.println(tempStringArray[i]);
+					String tempGram = "";
+					for(int j = 0; j<orderGram;j++){
+						if(i+j < tempStringArray.length){
+							tempGram += tempStringArray[i+j] + " ";
+							System.out.println("Inne i while loop");
+							if(tempStringArray[i+j].endsWith(".")){
+								break;
+							}
+						}
+					}
+					out.write(tempGram);
+					out.write("\n");
+				}
+				tempString = br.readLine();
+			}
+			br.close();
+			out.close();
+				
+		} catch(FileNotFoundException e){
+			System.out.println("Exception! " + e);
+		}
+	}
 }
