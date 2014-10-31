09f1c2ddfdad1f7e83c2773e27354452a37eff08
tryti <tryti@kth.se>
2014-10-31 15:32:34 +0100

    Added user and hashtag constants

--- IRTwitter/src/Constants.java
+++ IRTwitter/src/Constants.java
@@ -1,4 +1,6 @@
 public class Constants{
     public final static String KEYSEPATOR = "$";
     public final static String REGEX_SENTENCE_SPLIT = "\\s+";
+    public final static String HASHTAG = "$HASHTAG";
+    public final static String USERNAME = "$USERNAME";
 }
